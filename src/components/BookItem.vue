7ab1fda5bb28eb2c2384a8ab45a392b7781fc9ff
DeviNeerajaK <120345015+DeviNeerajaK@users.noreply.github.com>
2023-04-30 01:48:52 -0400

    vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div :class="book.available ? 'green': 'red' ">
+        <h2>Motel Name : {{book.motelname}}</h2>
+        <address> Address : {{book.address}}</address>
+        <address> Contact Us : {{book.number}}</address>
+        <h4> Total Rooms : {{book.totalrooms}}</h4>
+        <h4> Rooms Available : {{book.roomavailability}}</h4>
+        <h4> Cost : {{book.cost}}</h4>
+        <h4> Ratings : {{book.rating}}</h4>
+        
+        
+    </div>
+
+</template>
+
+<script>
+
+
+export default{
+    name: 'BookItem',
+    props:{
+        book: Array
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+
+h4{
+    margin: 2px;
+}
+
+div{
+    background-color: #ffb89e;
+    color: black;
+    padding: 1em;
+    text-align: center;
+}
+
+.red{
+background-color: orangered !important;
+
+}
+.green{
+    color: white;
+    background-color: green;
+}
+
+
+</style>
